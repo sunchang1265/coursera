90cd5d68ba08ac6af299563ba7fd68c0f6b91546
csun <sunchang1265@gmail.com>
2018-05-03 15:53:05 -0400

    Adding needed classes and libraries

---
+++ src/PercolationStats.java
@@ -0,0 +1,21 @@
+public class PercolationStats {
+    public PercolationStats(int n, int trials){
+
+    } // perform trials independent experiments on an n-by-n grid
+    public double mean() {
+        return 1.0;
+    } // sample mean of percolation threshold
+    public double stddev(){
+        return 1.0;
+    } // sample standard deviation of percolation threshold
+    public double confidenceLo() {
+        return 1.0;
+    } // low  endpoint of 95% confidence interval
+    public double confidenceHi(){
+        return 1.0;
+    } // high endpoint of 95% confidence interval
+
+    public static void main(String[] args) {
+
+    } // test client (described below)
+}
